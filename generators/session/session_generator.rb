84fb35324da2e89593115bc05a18308278458f8f
Jeremy Lecour <jeremy.lecour@gmail.com>
2009-09-09 11:18:00 +0200

    Renamed .../templates/session.{rb,erb} to validate the syntax check

--- generators/session/session_generator.rb
+++ generators/session/session_generator.rb
@@ -3,7 +3,7 @@ class SessionGenerator < Rails::Generator::NamedBase
     record do |m|
       m.class_collisions class_name
       m.directory File.join('app/models', class_path)
-      m.template 'session.rb', File.join('app/models', class_path, "#{file_name}.rb")
+      m.template 'session.erb', File.join('app/models', class_path, "#{file_name}.rb")
     end
   end
 end
